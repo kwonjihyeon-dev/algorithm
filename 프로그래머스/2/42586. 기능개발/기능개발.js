3a5f765badc5265c383f4d18ac036981b3902191
jihyeon <90013333+kwonjihyeon-dev@users.noreply.github.com>
2026-03-11 20:20:12 +0900

    [level 2] Title: 기능개발, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,38 +1,24 @@
 function solution(progresses, speeds) {
-    var answer = [];
-    const dueDates = [] // 이중배열? 
-
-    const calc = (process, plus) => {
-        if (process + plus >= 100) {
-            return 1;
+    let completed = 0;
+    let deployIdx = 0;
+    const deploy = [0];
+    
+    for (let i = 0; i < progresses.length; i++) {
+        const last = 100 - progresses[i];
+        const duration = Math.ceil(last / speeds[i]);
+        if (completed) {
+            if (duration > completed) {
+                completed = duration;
+                ++deployIdx;
+                deploy[deployIdx] = 1;
+            } else {
+                deploy[deployIdx]++;
+            }
         } else {
-            let test = 100 - process;
-            return Math.ceil(test / plus);
+            completed = duration;
+            deploy[deployIdx]++;
         }
     }
     
-    let stack = []; // 쌓기
-    let completed = []; // 배포되는 기능 수
-    
-    progresses.map((progress, i) => {
-        const result = calc(progress, speeds[i]);
-        console.log(result);
-        
-        if (stack.length) {
-            if (stack[0] >= result) {
-                // 앞의 기능 완료일이 더 크면 넣어주고,
-                stack.push(result);
-            } else {
-                // 앞의 기능 완료보다 지금 완료한 게 더 오래걸리면 비우고 다시 넣어주기
-                completed.push(stack.length);
-                stack = [];
-                stack.push(result);
-            }
-        } else {
-            stack.push(result)   
-        }
-    })
-    completed.push(stack.length);
-    // console.log(stack, completed)
-    return completed;
+    return deploy
 }
